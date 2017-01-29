e2dd7ffb5a6c293cbac1644237d911560826d530
Ferdinand Beyer <fb@fbeyer.com>
2017-01-29 16:06:13 +0100

    Create initial project structure

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject datalite "0.1.0-SNAPSHOT"
+  :description "Datomic-inspired database on top of SQLite"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.xerial/sqlite-jdbc "3.16.1"]])
---
+++ src/datalite/core.clj
@@ -0,0 +1,6 @@
+(ns datalite.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
