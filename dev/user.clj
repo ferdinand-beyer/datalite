815c523e8a0d3b64ce23c537028658387cd4b722
Ferdinand Beyer <fb@fbeyer.com>
2017-02-04 14:04:23 +0100

    Expose test.check in user.clj

--- dev/user.clj
+++ dev/user.clj
@@ -3,6 +3,9 @@
             [clojure.pprint :refer [pprint]]
             [clojure.reflect :refer [reflect]]
             [clojure.repl :refer [apropos dir doc find-doc pst source]]
+            [clojure.test.check :as tc]
+            [clojure.test.check.generators :as gen]
+            [clojure.test.check.properties :as prop]
             [clojure.tools.namespace.repl :refer [refresh refresh-all]]
             [datalite.api :as d]))
 
