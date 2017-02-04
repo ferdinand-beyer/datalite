1d63659fcb20ba95e644e8440fe267ebfb78a816
Ferdinand Beyer <fb@fbeyer.com>
2017-02-04 12:39:50 +0100

    Require REPL tools in user.clj

--- dev/user.clj
+++ dev/user.clj
@@ -1,7 +1,8 @@
 (ns user
-  (:require [clojure.java.javadoc :refer (javadoc)]
-            [clojure.pprint :refer (pprint)]
-            [clojure.reflect :refer (reflect)]
-            [clojure.tools.namespace.repl :refer (refresh refresh-all)]
+  (:require [clojure.java.javadoc :refer [javadoc]]
+            [clojure.pprint :refer [pprint]]
+            [clojure.reflect :refer [reflect]]
+            [clojure.repl :refer [apropos dir doc find-doc pst source]]
+            [clojure.tools.namespace.repl :refer [refresh refresh-all]]
             [datalite.api :as d]))
 
