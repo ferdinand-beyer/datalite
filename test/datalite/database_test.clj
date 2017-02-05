8fc098981045c41cfa9f2e18ad6dfe392a453e66
Ferdinand Beyer <fb@fbeyer.com>
2017-02-05 12:34:23 +0100

    Add test for non-unique attr in lookup ref

--- test/datalite/database_test.clj
+++ test/datalite/database_test.clj
@@ -25,6 +25,8 @@
                  (entity-id db [:db/ident :db.part/db 0])))
     (is (thrown? clojure.lang.ExceptionInfo
                  (entity-id db [:foo :bar])))
+    (is (thrown? clojure.lang.ExceptionInfo
+                 (entity-id db [:db/valueType :db.type/keyword])))
     (is (= schema/part-db (entity-id db [:db/ident :db.part/db])))
     (is (= schema/part-db (entity-id db [schema/ident :db.part/db])))))
 
