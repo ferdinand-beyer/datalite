7420a5443abc703efa15d0cf4dc1c321f9095ef6
Ferdinand Beyer <fb@fbeyer.com>
2017-02-09 22:11:04 +0100

    Depend on SLF4J, use Logback for development

--- project.clj
+++ project.clj
@@ -3,11 +3,12 @@
   :url "https://github.com/ferdinand-beyer/datalite"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[ch.qos.logback/logback-classic "1.1.9"]
-                 [org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.logging "0.3.1"]
+                 [org.slf4j/slf4j-api "1.7.22"]
                  [org.xerial/sqlite-jdbc "3.16.1"]]
   :profiles {:dev {:source-paths ["dev"]
-                   :dependencies [[org.clojure/test.check "0.9.0"]
+                   :dependencies [[ch.qos.logback/logback-classic "1.1.9"]
+                                  [org.clojure/test.check "0.9.0"]
                                   [org.clojure/tools.namespace "0.2.11"]]}})
 
